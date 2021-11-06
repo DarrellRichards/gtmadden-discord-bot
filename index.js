3e19e9259979ee2ddec39074cfba9ff2c7227adf
Darrell Richards <darrell.richards@softrams.com>
2021-11-06 16:43:06 -0400

    added support for rookie tracking

--- index.js
+++ index.js
@@ -13,6 +13,35 @@ const TOKEN = process.env.TOKEN;
 const bot = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 bot.login(TOKEN);
 
+
+const grabRookies = async (msg, league, server) => {
+    const data = await axios.get(`https://gametime-21.herokuapp.com/${league}/rookie/stats`)
+    const element = msg.content.substr("!rookies ".length);
+    switch (element) {
+        case 'passing':
+            let embed = new Discord.MessageEmbed()
+            .setTitle('Rooking Passing Leaders')
+            for (const player of data.data.passingLeaders) {
+                // firstName, lastName, position, teamName
+                // stats = passAtt, passComp, passInts, passTDs, passYds
+                embed.addField(`${player.firstName} ${player.lastName}, ${player.position}, ${player.teamName}`, `${player.stats.passComp}/${player.stats.passAtt} , ${player.stats.passInts} Ints, ${player.stats.passTDs} TD's, ${player.stats.passYds} Yards`)
+                
+            }
+            if (embed) msg.reply({embeds: [embed]})
+            // msg.reply(embed)
+            break;
+    
+        default:
+            break;
+    }
+    // const list = data.data.map((item, i) => {
+    //     console.log(item)
+    //     return
+    // })
+    // console.log(data.data.passingLeaders)
+
+}
+
 bot.on('ready', async () => {
   console.info(`Logged in as ${bot.user.tag}!`);
   await client.connect()
@@ -23,6 +52,12 @@ bot.on('message', async (msg) => {
         let cat = 0;
         let league = '6214563'
         const server = msg.guild.id;
+        const leagueInfo = await client.query(`SELECT * FROM gtmadden WHERE server=${server}`)
+        if (leagueInfo.rowCount > 0) {
+            for( const item of leagueInfo.rows) {
+                league = item.league
+            }
+        }
         if (msg.content.startsWith('!config')) {
             if (msg.member.roles.cache.some(role => role.name === 'Channel Creator')) {
                 const leagueElement = msg.content.substr("!config ".length);
@@ -39,14 +74,11 @@ bot.on('message', async (msg) => {
                 return        
             }
         }
+        if (msg.content.startsWith('!rookies')) {
+            grabRookies(msg, league, server)
+        }
         if (msg.content.startsWith('!advanced')) {
             const week = msg.content.substr("!advanced ".length);
-            const leagueInfo = await client.query(`SELECT * FROM gtmadden WHERE server=${server}`)
-            if (leagueInfo.rowCount > 0) {
-                for( const item of leagueInfo.rows) {
-                    league = item.league
-                }
-            }
             if (msg.member.roles.cache.some(role => role.name === 'Channel Creator')) {
                 if (week > 0) {
                     msg.guild.channels.cache.forEach((channel)=>{
